9865fc48d252b2ea454ea461ec5c6b7b94f29ded
kglowa <93885006+kglowa@users.noreply.github.com>
2022-01-19 02:00:54 +0100

    Haptics

---
+++ HomeScreen.js
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { View, Text,Image, Button,SafeAreaView, StyleSheet, ScrollView, Alert, StatusBar } from 'react-native';
+import * as Haptics from 'expo-haptics';
+
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+
+
+const HomeScreen = ({ navigation }) => {
+  
+
+  return (
+   
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <SafeAreaView style={styles.container}>
+      <Image
+      style={{width:300,height:200,marginLeft:10,bottom:100,marginTop:120}}
+      resizeMode = "contain"
+      
+      source = {require('../icons/logo.png')}
+      />
+        <View>
+          <Button
+            title="Menu"
+            color="#d94214"
+            onPress={() => {Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);navigation.navigate("Menu")}}
+          />
+        </View>
+        <Separator />
+        <View>
+
+          <Button
+            title="Rezerwacje"
+            color="#d94214"
+            onPress={() => {Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);navigation.navigate("Rezerwacje")}}
+          />
+        </View>
+        <Separator />
+        <View>
+
+          <Button
+            title="Zamowienia"
+            color="#d94214"
+            onPress={() =>{
+              navigation.navigate("Zamowienia");
+              Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)}}
+          />
+        </View>
+        <Separator />
+
+        <View>
+
+          <Button
+            title="Rachunki"
+            color="#d94214"
+            onPress={() => {Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);navigation.navigate("Rachunki")}}
+          />
+        </View>
+        <Separator />
+        
+        <View>
+          <Button
+            title="Czas pracy"
+            color="#d94214"
+            onPress={() => {Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);navigation.navigate("Czas pracy")}}
+          />
+        </View>
+        <Separator />
+        <View>
+          <Button
+            title="Wyloguj"
+            color="#d94214"
+            onPress={() => {Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);navigation.navigate("Login")}}
+
+            />
+        </View>
+        <Separator />
+      </SafeAreaView>
+    </View>
+  );
+
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  
+});
+export default HomeScreen;
